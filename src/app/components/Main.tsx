2269a891f55eb8c82288d334422491bd354104ae
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-04-10 17:22:15 +0530

    Timeline

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -3,45 +3,18 @@ import Footer from "./Footer";
 import Hero from "./Hero"
 import Intro from "./Intro";
 import PhaseOne from "./PhaseOne";
-import { motion } from "motion/react"
+import { Timeline } from "./Timeline";
 const Main = () => {
-
-    const pageVariants = {
-        initial: {
-            opacity: 0,
-        },
-        animate: {
-            opacity: 1,
-            transition: {
-                duration: 0.6,
-                ease: "easeOut",
-                staggerChildren: 0.3
-            }
-        },
-        exit: {
-            opacity: 0,
-            y: 4,
-            transition: {
-                duration: 0.4,
-                ease: "easeIn"
-            }
-        }
-    };
     return (
-        <motion.div
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={pageVariants}
-            className="w-full min-h-screen"
-        >
+        <div>
             <Hero />
             <Intro />
             <PhaseOne />
+            <Timeline/>
             <Final />
             <Footer />
             
-        </motion.div>
+        </div>
     )
 }
 export default Main;
